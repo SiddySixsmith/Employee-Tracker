2fae98e472bc83bd3a0e10c35b9be6c54c2aa016
Siddy <dancore99@outlook.com>
2021-11-14 08:07:07 +0800

    completion of sql files

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,41 @@ CREATE DATABASE Employee_tracker_db;
 
 USE Employee_tracker_db;
 
-CREATE TABLE department {
+CREATE TABLE department (
     id INT  AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
-};
+);
 
-CREATE TABLE role {
-    id INT PRIMARY KEY,
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-};
+);
 
-CREATE TABLE employee {
-    id INT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-};
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee (id)
+);
+
+ALTER TABLE employee 
+ADD CONSTRAINT 'employee_role'
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION,
+ADD CONSTRAINT 'manager_id'
+FOREIGN KEY (manager_id)
+REFERENCES employee (id)
+ON DELETE RESTRICT
+ON UPDATE NO ACTION;
--- server.js
+++ server.js
@@ -1,11 +1,65 @@
 const inquirer = require("inquirer")
-const mysql = require("mysql")
+const mysql = require("mysql2")
 const cTable = require('console.table');
+const db = require("./config/connection")
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
-    user: "root",
-    password: "Dannyboy1!",
-    database: "employee_tracker_db"
+const PORT = process.env.PORT || 3306;
+
+  db.connect((err) => {
+  if (err) {
+    console.log("Database Connection Failed !!!", err);
+  } else {
+    console.log("connected to Database");
+    startApp();
+  }
+});
+
+function getDepartmentTable(){
+  db.promise().query("SELECT * FROM department;")
+  .then(([data]) => {
+      console.log(data);
+      startApp();
+  })
+  .catch(console.log(data));
+};
+
+
+function getRoleTable(){
+  db.query(`SELECT * FROM role;`, (err, results) => {
+    console.table(results)
+  }).catch(err)
+  console.log(err);
+};
+
+function getEmployeeTable(){
+  db.query(`SELECT * FROM employee;`, function (err, results) {
+    console.table(results);
+  }).catch(err)
+  console.log(err);
+   startApp();
+};
+
+const MainQuestions = 
+  {
+    type: "list",
+    message: "What would you like to do",
+    choices: [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add new department',
+    'Add new role',
+    'Add new employee',
+    'Update employee role'
+  ],
+    name: "home_Questions"
+  };
+  
+async function startApp(){
+await inquirer.prompt(MainQuestions)
+  .then((result) => {
+    if (result.choice ==="View all employees?") {
+          getEmployeeTable();
+    }
   });
+}
